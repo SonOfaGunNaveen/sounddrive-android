5b6e74da1d72a41f4e08da6cf3a8871bb34ba98a
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 12:57:42 +0530

    Remove Android configuration from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,17 @@
 plugins {
+
     id("com.android.application")
+        version "8.5.2"
+        apply false
+
     kotlin("android")
+        version "2.0.21"
+        apply false
+
     kotlin("kapt")
-    id("com.google.dagger.hilt.android")
-}
-
-android {
-
-    namespace = "com.sounddrive"
-
-    compileSdk = 35
-
-    defaultConfig {
-
-        applicationId = "com.sounddrive"
-
-        minSdk = 26
-        targetSdk = 35
-
-        versionCode = 1
-        versionName = "0.1.0"
-    }
-
-    buildTypes {
-
-        release {
-            isMinifyEnabled = false
-        }
-
-        debug {
-            isDebuggable = true
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility =
-            JavaVersion.VERSION_17
-
-        targetCompatibility =
-            JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-
-    implementation(
-        "androidx.core:core-ktx:1.15.0"
-    )
-
-    implementation(
-        "androidx.appcompat:appcompat:1.7.0"
-    )
-
-    implementation(
-        "com.google.android.material:material:1.12.0"
-    )
-
-    implementation(
-        "androidx.media3:media3-exoplayer:1.5.1"
-    )
-
-    implementation(
-        "androidx.media3:media3-session:1.5.1"
-    )
-
-    implementation(
-        "androidx.lifecycle:lifecycle-runtime-ktx:2.8.7"
-    )
-
-    implementation(
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0"
-    )
-
-    implementation(
-        "com.google.dagger:hilt-android:2.56.2"
-    )
-
-    kapt(
-        "com.google.dagger:hilt-compiler:2.56.2"
-    )
-}
-
-kapt {
     correctErrorTypes = true
+
+    id("com.google.dagger.hilt.android")
+        version "2.56.2"
+        apply false
 }
