f666edd43fb5ca0b0766199b6a09ae8294df4ef5
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 12:10:25 +0530

    Add PlaybackState data class for audio playback

---
+++ app/src/main/java/com/sounddrive/audio/PlaybackState.kt
@@ -0,0 +1,11 @@
+package com.sounddrive.audio
+
+data class PlaybackState(
+
+    val currentBpm: Int = 120,
+
+    val currentMode: AudioMode =
+        AudioMode.AMBIENT,
+
+    val currentBar: Long = 0
+)
