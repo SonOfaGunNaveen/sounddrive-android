858ba2f45920db5b21815a407b559b63fb4ae5db
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 23:17:45 +0530

    Refactor plugins section in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,7 @@
 plugins {
+    id("com.android.application") version "8.5.2" apply false
 
-    id("com.android.application")
-        version "8.5.2"
-        apply false
+    kotlin("android") version "2.0.21" apply false
 
-    kotlin("android")
-        version "2.0.21"
-        apply false
-
-    id("com.google.dagger.hilt.android")
-        version "2.56.2"
-        apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
