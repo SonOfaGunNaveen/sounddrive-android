8c0b39515de2c897c7514e08bc41d0e0ebb3152c
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 12:21:28 +0530

    Add AudioProfile data class for audio multipliers

---
+++ app/src/main/java/com/sounddrive/audio/AudioProfile.kt
@@ -0,0 +1,12 @@
+package com.sounddrive.audio
+
+data class AudioProfile(
+
+    val drumsMultiplier: Float,
+
+    val bassMultiplier: Float,
+
+    val synthMultiplier: Float,
+
+    val fxMultiplier: Float
+)
