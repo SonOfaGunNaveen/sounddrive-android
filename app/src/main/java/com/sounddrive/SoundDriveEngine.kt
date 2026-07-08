e0244d2dd6563c6c032a733b3577b18eef9d97ce
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 12:43:44 +0530

    Update SoundDriveEngine.kt

--- app/src/main/java/com/sounddrive/SoundDriveEngine.kt
+++ app/src/main/java/com/sounddrive/SoundDriveEngine.kt
@@ -1,30 +1,11 @@
 package com.sounddrive
 
-import com.sounddrive.audio.AdaptiveAudioEngine
-import com.sounddrive.audio.AudioOrchestrator
-import com.sounddrive.audio.StemMixer
-import com.sounddrive.telemetry.DrivingAnalyzer
-
 class SoundDriveEngine {
-
-    private val analyzer =
-        DrivingAnalyzer()
-
-    private val adaptiveEngine =
-        AdaptiveAudioEngine()
-
-    private val stemMixer =
-        StemMixer()
-
-    private val audioOrchestrator =
-        AudioOrchestrator(
-            analyzer,
-            adaptiveEngine,
-            stemMixer
-        )
 
     fun start() {
 
-        audioOrchestrator.start()
+        println(
+            "SoundDrive Engine Started"
+        )
     }
 }
