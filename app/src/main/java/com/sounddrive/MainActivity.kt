e7891341135b02aaac3db595c8003ff76f5ebaea
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 12:37:28 +0530

    Add MainActivity class with SoundDriveEngine initialization

---
+++ app/src/main/java/com/sounddrive/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sounddrive
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+
+        val engine = SoundDriveEngine()
+
+        engine.start()
+    }
+}
