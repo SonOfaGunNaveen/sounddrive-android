2f35b442eb9c9e88a53362aa25d433d0dd60771f
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-08 11:39:23 +0530

    Add AudioLayer data class for audio gains

---
+++ app/src/main/java/com/sounddrive/audio/AudioLayer.kt
@@ -0,0 +1,12 @@
+package com.sounddrive.audio
+
+data class AudioLayer(
+
+    val drumsGain: Float = 0f,
+
+    val bassGain: Float = 0f,
+
+    val synthGain: Float = 0f,
+
+    val fxGain: Float = 0f
+)
