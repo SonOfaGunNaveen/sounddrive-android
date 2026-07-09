2d0e47917ede408ead517b7812a24835113cf1a3
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-09 09:25:07 +0530

    Add VehicleData data class for telemetry

---
+++ app/src/main/java/com/sounddrive/telemetry/VehicleData.kt
@@ -0,0 +1,12 @@
+package com.sounddrive.telemetry
+
+data class VehicleData(
+
+    val rpm: Int = 0,
+
+    val throttle: Float = 0f,
+
+    val coolantTemp: Float = 0f,
+
+    val fuelLevel: Float = 0f
+)
