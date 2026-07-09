af0c8c6e16ddfaf7cf78297a2a6c7763459aca27
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-09 09:39:52 +0530

    Add MixStatistics data class for audio mixing

---
+++ app/src/main/java/com/sounddrive/audio/MixStatistics.kt
@@ -0,0 +1,12 @@
+package com.sounddrive.audio
+
+data class MixStatistics(
+
+    val averageDrums: Float,
+
+    val averageBass: Float,
+
+    val averageSynth: Float,
+
+    val averageFx: Float
+)
