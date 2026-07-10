d85300721a2a69093f3e9a8fd4b942c8c31b0ce5
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-10 22:06:33 +0530

    Refactor GpsSpeedProvider to use Context and listener

--- app/src/main/java/com/sounddrive/telemetry/GpsSpeedProvider.kt
+++ app/src/main/java/com/sounddrive/telemetry/GpsSpeedProvider.kt
@@ -1,12 +1,48 @@
 package com.sounddrive.telemetry
 
-class GpsSpeedProvider {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 
-    fun start() {
+class GpsSpeedProvider(
+    private val context: Context
+) {
 
-    }
+    private var speedListener:
+        ((Float) -> Unit)? = null
 
-    fun stop() {
+    @SuppressLint("MissingPermission")
+    fun start(
+        listener: (Float) -> Unit
+    ) {
 
+        speedListener = listener
+
+        val locationManager =
+            context.getSystemService(
+                Context.LOCATION_SERVICE
+            ) as LocationManager
+
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1000L,
+            1f,
+            object : LocationListener {
+
+                override fun onLocationChanged(
+                    location: Location
+                ) {
+
+                    val speedKmh =
+                        location.speed * 3.6f
+
+                    speedListener?.invoke(
+                        speedKmh
+                    )
+                }
+            }
+        )
     }
 }
