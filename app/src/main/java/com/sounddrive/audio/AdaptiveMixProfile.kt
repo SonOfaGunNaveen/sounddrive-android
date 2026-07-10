5501cf34ba106b49521c8869753503cc401274fe
SonOfaGunNaveen <cnaveenkumar78@gmail.com>
2026-07-10 21:40:01 +0530

    Create AdaptiveMixProfile.kt

---
+++ app/src/main/java/com/sounddrive/audio/AdaptiveMixProfile.kt
@@ -0,0 +1,14 @@
+package com.sounddrive.audio
+
+data class AdaptiveMixProfile(
+
+    val drumsMultiplier: Float,
+
+    val bassMultiplier: Float,
+
+    val padsMultiplier: Float,
+
+    val arpMultiplier: Float,
+
+    val fxMultiplier: Float
+)
